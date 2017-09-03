e7a296215034ff77a30aa503e50795eab8900132
MrF83 <16142484+MrF83@users.noreply.github.com>
2017-09-03 11:33:15 +0200

    Updated ChainRobot
    Improved ChainRobot model behavior.

--- MrF83RobotCode/ChainRobot.py
+++ MrF83RobotCode/ChainRobot.py
@@ -9,11 +9,12 @@ class ChainRobot(AbstractRobot):
         self.DriveMotorL = LargeMotor("outA")
         self.DriveMotorR = LargeMotor("outB")
         self.VerticalMotor = MediumMotor("outC")
+        self.running = [False, ""]
 
     def Stop(self):
         self.DriveMotorL.stop(stop_action="coast")
         self.DriveMotorR.stop(stop_action="coast")
-        self.VerticalMotor.stop(stop_action="coast")
+        self.VerticalMotor.stop(stop_action="hold")
 
     def React(self, msg):
 
@@ -21,21 +22,35 @@ class ChainRobot(AbstractRobot):
         self.SetSpeeds(decodedMsg)
 
         if decodedMsg["State"]  == "P":
-            if decodedMsg["Key"] == "D":
-                self.DriveMotorL.run_forever(speed_sp = self.CurveInnerMotorSpeed)
-                self.DriveMotorR.run_forever(speed_sp = self.DriveSpeed)
+            if decodedMsg["Key"] == "A":
+                if self.running[1] == "W" or self.running[1] == "":
+                    self.DriveMotorL.run_forever(speed_sp = -self.CurveInnerMotorSpeed)
+                    self.DriveMotorR.run_forever(speed_sp = -self.DriveSpeed)
+                elif self.running[1] == "S":
+                    self.DriveMotorR.run_forever(speed_sp = self.CurveInnerMotorSpeed)
+                    self.DriveMotorL.run_forever(speed_sp = self.DriveSpeed)
+                else:
+                    return
+
+            elif decodedMsg["Key"] == "D":
+                if self.running[1] == "W" or self.running[1] == "":
+                    self.DriveMotorL.run_forever(speed_sp = -self.DriveSpeed)
+                    self.DriveMotorR.run_forever(speed_sp = -self.CurveInnerMotorSpeed)
+                elif self.running[1] == "S":
+                    self.DriveMotorR.run_forever(speed_sp = self.DriveSpeed)
+                    self.DriveMotorL.run_forever(speed_sp = self.CurveInnerMotorSpeed)
+                else:
+                    return
 
             elif decodedMsg["Key"] == "W":
                 self.DriveMotorL.run_forever(speed_sp = -self.DriveSpeed)
                 self.DriveMotorR.run_forever(speed_sp = -self.DriveSpeed)
-
-            elif decodedMsg["Key"] == "A":
-                self.DriveMotorL.run_forever(speed_sp = self.DriveSpeed)
-                self.DriveMotorR.run_forever(speed_sp = self.CurveInnerMotorSpeed)
+                self.running = [True, "W"]
 
             elif decodedMsg["Key"] == "S":
                 self.DriveMotorL.run_forever(speed_sp = self.DriveSpeed)
                 self.DriveMotorR.run_forever(speed_sp = self.DriveSpeed)
+                self.running = [True, "S"]
 
             elif decodedMsg["Key"] == "Q":
                 self.VerticalMotor.run_forever(speed_sp = self.VerticalTurnSpeed)
@@ -48,20 +63,36 @@ class ChainRobot(AbstractRobot):
 
         elif decodedMsg["State"]  == "R":
             if decodedMsg["Key"] == "A":
-                self.DriveMotorL.stop(stop_action="coast")
-                self.DriveMotorR.stop(stop_action="coast")
+                if self.running[0] and self.running[1] == "W":
+                    self.DriveMotorL.run_forever(speed_sp = -self.DriveSpeed)
+                    self.DriveMotorR.run_forever(speed_sp = -self.DriveSpeed)
+                elif self.running[0] and self.running[1] == "S":
+                    self.DriveMotorL.run_forever(speed_sp = self.DriveSpeed)
+                    self.DriveMotorR.run_forever(speed_sp = self.DriveSpeed)
+                else:
+                    self.DriveMotorL.stop(stop_action="coast")
+                    self.DriveMotorR.stop(stop_action="coast")
 
+            elif decodedMsg["Key"] == "D":
+                if self.running[0] and self.running[1] == "W":
+                    self.DriveMotorL.run_forever(speed_sp = -self.DriveSpeed)
+                    self.DriveMotorR.run_forever(speed_sp = -self.DriveSpeed)
+                elif self.running[0] and self.running[1] == "S":
+                    self.DriveMotorL.run_forever(speed_sp = self.DriveSpeed)
+                    self.DriveMotorR.run_forever(speed_sp = self.DriveSpeed)
+                else:
+                    self.DriveMotorL.stop(stop_action="coast")
+                    self.DriveMotorR.stop(stop_action="coast")
+                
             elif decodedMsg["Key"] == "S":
                 self.DriveMotorL.stop(stop_action="coast")
                 self.DriveMotorR.stop(stop_action="coast")
-
-            elif decodedMsg["Key"] == "D":
-                self.DriveMotorL.stop(stop_action="coast")
-                self.DriveMotorR.stop(stop_action="coast")
+                self.running = [False, ""]
 
             elif decodedMsg["Key"] == "W":
                 self.DriveMotorL.stop(stop_action="coast")
                 self.DriveMotorR.stop(stop_action="coast")
+                self.running = [False, ""]
 
             elif decodedMsg["Key"] == "Q":
                 self.VerticalMotor.stop(stop_action="hold")
